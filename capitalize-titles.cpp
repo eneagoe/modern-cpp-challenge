409c435bfe8696651cc5b3af2c493ff979741c90
eugen neagoe <eneagoe@gmail.com>
2020-05-12 22:47:02 +0300

    Add function to capitalize title strings

---
+++ capitalize-titles.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+template <class Elem>
+using tstring = std::basic_string<Elem, std::char_traits<Elem>, std::allocator<Elem>>;
+
+template <class Elem>
+using tstringstream = std::basic_stringstream<Elem, std::char_traits<Elem>, std::allocator<Elem>>;
+
+template <class Elem>
+tstring<Elem> capitalize(tstring<Elem> const & text)
+{
+    tstringstream<Elem> result;
+    bool newWord = true;
+
+    for (auto const ch : text)
+    {
+        newWord = newWord || std::ispunct(ch) || std::isspace(ch);
+        if (std::isalpha(ch)) {
+            if (newWord) {
+                result << static_cast<Elem>(std::toupper(ch));
+                newWord = false;
+            } else
+                result << static_cast<Elem>(std::tolower(ch));
+        } else
+            result << ch;
+    }
+
+    return result.str();
+}
+
+int main(void)
+{
+    using namespace std::string_literals;
+
+    assert("The C++ Challenger"s == capitalize("the c++ challenger"s));
+    assert("This Is An Example, Should Work!"s == capitalize("THIS IS an ExamplE, should wORk!"s));
+
+    return 0;
+}
+
